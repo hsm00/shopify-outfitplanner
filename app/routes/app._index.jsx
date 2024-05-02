a3a7cb9fe0254bbe3b2fb7b94f0a7fffefa546d4
Hesam <106731623+hsm00@users.noreply.github.com>
2024-05-02 16:31:35 +0200

    fixed the host issue

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -16,8 +16,8 @@ import {
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
-
+  const { admin, session } = await authenticate.admin(request);
+  console.log(session.accessToken);
   return null;
 };
 
--- app/routes/app.outfit.jsx
+++ app/routes/app.outfit.jsx
@@ -1,69 +1,49 @@
 import { json } from "@remix-run/node";
 import db from "../db.server";
-import {authenticate, unauthenticated} from "../shopify.server.js";
+import { apiVersion, authenticate } from "../shopify.server";
 
-export const loader = async ({ request }) => {
-  const customerId = "7539008241822";
-  const shop = "hesams-outfitplanner.myshopify.com";
-  const pantsName = "Levis Jeans"
-  const pantsImageUrl = "https://cdn.shopify.com/s/files/1/0645/0328/3870/products/snowboard_wax.png?v=1708976930&width=713"
-  const topName = "T-shirt"
-  const topImageUrl = "https://cdn.shopify.com/s/files/1/0645/0328/3870/products/snowboard_wax.png?v=1708976930&width=713"
-  const shoesName = "Sneakers"
-  const shoesImageUrl = "https://cdn.shopify.com/s/files/1/0645/0328/3870/products/snowboard_wax.png?v=1708976930&width=713"
-  const accessoriesName = "Watch"
-  const accessoriesImageUrl = "https://cdn.shopify.com/s/files/1/0645/0328/3870/products/snowboard_wax.png?v=1708976930&width=713"
 
-  console.log("REQUEST", request);
-  const { admin } = await authenticate.admin(request);
-
-  console.log(admin);
-
-  const outfit = await db.outfit.findMany(
-    {
-      where: {
-        userId: customerId,
-        shop: shop,
-      },
-    }
-  )
-
-  const response = await admin.graphql(
-    `#graphql
-  query getProductById($id: ID!) {
-    product(id: $id) {
-      title
-    }
-  }`,
-    {
-      variables: {
-        id: "gid://shopify/Product/7676292956318"
+export const query =`
+{
+    products(first: 10) {
+      edges{
+        node{
+          title
+        }
       }
     }
-  );
+}
+`
+export const loader = async ({ request }) => {
+  const shop = "hesams-outfitplanner.myshopify.com";
 
-    console.log(response);
+  try{
+    const response = await fetch(`https://${shop}/admin/api/${apiVersion}/graphql.json`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/graphql",
+        "X-Shopify-Access-Token": request.headers.get("X-Shopify-Access-Token"),
+  },
+    body: query
 
-  const userId = outfit[0].userId;
-  const shopUrl = outfit[0].shop;
-  const pantsId = outfit[0].accessoriesId;
-
-
-  if(outfit.length === 0) {
-    return json({
-      name: "No outfit found",
-    });
-  }
-  return json({
-    name: outfit[0].name,
-    description: outfit[0].description,
-    pants_name: pantsName,
-    pants_image_url: pantsImageUrl,
-    top_name: topName,
-    top_image_url: topImageUrl,
-    shoes_name: shoesName,
-    shoes_image_url: shoesImageUrl,
-    accessories_name: accessoriesName,
-    accessories_image_url: accessoriesImageUrl,
   });
-};
+
+    if(response.ok){
+      const data = await response.json()
+
+      const {
+        data: {
+          products: { edges }
+        }
+      } = data;
+      console.log(edges)
+      return edges
+    }
+
+    return null
+
+  } catch(err){
+    console.log(err)
+  }
+
+}
